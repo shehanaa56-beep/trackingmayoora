32b12b4e5f7680f4aaafddc94ac67467ebb412ac
shehanaa56-beep <shehanaa56@gmail.com>
2026-01-06 14:24:18 +0530

    Fix blank page by removing service worker registration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,22 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="header">
+      
+      {/* LOGO IMAGE */}
       <div className="logo">
-        <h1>RyZenSports</h1>
+        <img
+          src="/images/ryzen.png"
+          alt="RyZenSports Logo"
+          className="logo-img"
+        />
       </div>
+
       <nav>
-        <Link to="/admin" className="admin-link">Admin Login</Link>
+        <Link to="/admin" className="admin-link">
+          <i className="bi bi-person-circle"></i>
+        </Link>
       </nav>
+
     </header>
   );
 };
